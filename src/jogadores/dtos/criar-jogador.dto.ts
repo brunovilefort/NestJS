ce2c8f4eed18ea3a649b0d28df33cf64a8ae32e6
Bruno Vilefort <bruno.clara@yahoo.com>
2022-12-05 22:21:20 -0300

    feat: create jogador dto

---
+++ src/jogadores/dtos/criar-jogador.dto.ts
@@ -0,0 +1,5 @@
+export class CriarJogadorDto {
+  readonly phone: string;
+  readonly email: string;
+  readonly nome: string;
+}
--- src/jogadores/jogadores.controller.ts
+++ src/jogadores/jogadores.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CriarJogadorDto } from './dtos/criar-jogador.dto';
 
 @Controller('api/jogadores')
 export class JogadoresController {
   @Post()
-  async criarAtualizarJogadores() {
-    return JSON.stringify({ nome: 'Diego' });
+  async criarAtualizarJogadores(
+    @Body()
+    criarJogadorDto: CriarJogadorDto,
+  ) {
+    const { email } = criarJogadorDto;
+    return JSON.stringify(`{ "email": ${email} }`);
   }
 }
