7ac92a66457319ea41c4d18b73a098e2a1220173
Bruno Vilefort <bruno.clara@yahoo.com>
2022-12-07 11:08:56 -0300

    feat: create categories module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,14 @@ import 'dotenv/config';
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PlayersModule } from './players/players.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGO_URI), PlayersModule],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGO_URI),
+    PlayersModule,
+    CategoriesModule,
+  ],
   controllers: [],
   providers: [],
 })
