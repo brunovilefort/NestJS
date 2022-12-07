e7d8200d22a11b5b6e30bc4bee3361cb1ba628d5
Bruno Vilefort <bruno.clara@yahoo.com>
2022-12-07 11:39:45 -0300

    feat: create categories service

--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { CategoriesController } from './categories.controller';
 import { CategoriesService } from './categories.service';
+import { CategorieSchema } from './interfaces';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Categorie', schema: CategorieSchema }]),
+  ],
   controllers: [CategoriesController],
   providers: [CategoriesService],
 })
--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,4 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
 
+import { ICategories } from './interfaces';
+import { CreateCategorieDTO } from './dtos';
 @Injectable()
-export class CategoriesService {}
+export class CategoriesService {
+  constructor(
+    @InjectModel('Categorie')
+    private readonly categorieModel: Model<ICategories>,
+  ) {}
+
+  async create({
+    categorie,
+    description,
+    events,
+  }: CreateCategorieDTO): Promise<ICategories> {
+    const existingCategorie = await this.categorieModel
+      .findOne({ categorie })
+      .exec();
+    if (existingCategorie) {
+      throw new BadRequestException(
+        `Categorie: ${categorie} is already registered.`,
+      );
+    }
+    const createdCategorie = new this.categorieModel(
+      categorie,
+      description,
+      events,
+    );
+    return await createdCategorie.save();
+  }
+}
