fd436914423d04f20ccecf733e92197a2f351899
Bruno Vilefort <bruno.clara@yahoo.com>
2022-12-07 16:26:52 -0300

    feat: create challenge module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
 import { CategoriesModule, PlayersModule } from './index';
+import { ChallengeModule } from './challenge/challenge.module';
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGO_URI), PlayersModule, CategoriesModule],
+  imports: [MongooseModule.forRoot(process.env.MONGO_URI), PlayersModule, CategoriesModule, ChallengeModule],
   controllers: [],
   providers: [],
 })
