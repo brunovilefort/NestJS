cf1f8d10e840e144466d7c0f1a449fa6f77fd29d
Bruno Vilefort <bruno.clara@yahoo.com>
2022-12-08 01:53:25 -0300

    feat: add AllExceptionsFilter and moment-timezone to main file

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import * as momentTimezone from 'moment-timezone';
+
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './filters';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new AllExceptionsFilter());
+  Date.prototype.toJSON = function (): any {
+    return momentTimezone(this)
+      .tz('America/Sao_Paulo')
+      .format('YYYY-MM-DD HH:mm:ss.SSS');
+  };
   await app.listen(3000);
 }
 bootstrap();
