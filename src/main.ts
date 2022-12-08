5e6e4eff2719b96eb839a1008501e739b4e86db5
Bruno Vilefort <bruno.clara@yahoo.com>
2022-12-07 21:58:26 -0300

    chore: add moment-timezone package

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import * as momentTimezone from 'moment-timezone';
+
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './common/filters';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new AllExceptionsFilter());
+  Date.prototype.toJSON = function (): any {
+    return momentTimezone(this).tz('America/Sao_Paulo').format('YYYY-MM-DD HH:mm:ss.SSS');
+  };
   await app.listen(8080);
 }
 bootstrap();
